1298b750a29187fcfeca4e2b8319c07fbbeebe2f
sqroc <sqrocer@gmail.com>
2012-08-31 11:08:50 +0800

    add login_view

--- application/views/login.php
+++ application/views/login.php
@@ -1,8 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 		<title>登录</title>
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<script src="js/bootstrap.min.js"></script>
 		<base href="<?=base_url() ?>"/>
 		<script type="text/javascript" src="<?=base_url() ?>js/jquery-1.4.1.min.js"></script>
 	</head>
